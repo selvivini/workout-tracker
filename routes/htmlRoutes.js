64bf9a7e01488f80bbfb21f7f8149d5269488724
vinitha <selvivini@gmail.com>
2021-04-12 10:58:11 -0500

    added comments

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,12 @@
 const path= require('path');
 
 module.exports = function(app){
-
+//to get stats page
 app.get('/stats', (req,res)=>{
     res.sendFile(path.join(__dirname, '../public/stats.html'))
 })
 
+//gets the exercise page
 app.get('/exercise', (req,res)=>{
     res.sendFile(path.join(__dirname,'../public/exercise.html'))
 }
